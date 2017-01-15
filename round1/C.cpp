5697d13fc7b540b85216dd11c6a5a2bcd20d83d4
jlgm <jlgm@cin.ufpe.br>
2017-01-15 14:40:10 -0300

    input/output for problem C, won't be accepted

--- round1/C.cpp
+++ round1/C.cpp
@@ -18,14 +18,25 @@ ll n, m, k, adj[105][105], dist[105][105], memo[105][5005];
 pii f[5005];
 
 ll solve(ll prev, ll v) {
-    if (v == k) return 0;
+    //db(prev) db(v);
+    if (v >= k) return 0;
     if (v == k-1) return dist[prev][f[v].X] + dist[f[v].X][f[v].Y];
     if (memo[prev][v] != -1) return memo[prev][v];
     else {
         ll ans1 = dist[prev][f[v].X] + dist[f[v].X][f[v].Y] + solve(f[v].Y, v+1);
-        ll ans2 = dist[prev][f[v].X] + dist[f[v].X][f[v+1].X] +
-                    dist[f[v+1].X][f[v].Y] + dist[f[v].Y][f[v+1].Y] + solve(f[v+1].Y, v+2);
-        return memo[prev][v] = min(ans1, ans2);
+        ll ans2 = dist[prev][f[v].X] + dist[f[v].X][f[v+1].X]
+                    + dist[f[v+1].X][f[v].Y] + dist[f[v].Y][f[v+1].Y] + solve(f[v+1].Y,v+2);
+
+        for(int i = 3; i < k-v; i++) {
+            ll ans = dist[prev][f[v].X] + dist[f[v].X][f[v+1].X];
+            for (int j = 0; j < i-2; j++) {
+                ans += dist[f[v+j+1].X][f[v+j].Y] + dist[f[v+j].Y][f[v+j+2].X];
+            }
+            ans += dist[f[v+i-1].X][f[v+i-2].Y] + dist[f[v+i-2].Y][f[v+i-1].Y];
+            ans += solve(f[v+i-1].Y, v+i);
+            ans2 = min(ans2, ans);
+        }
+        return memo[prev][v] = min(ans1,ans2);
     }
 }
 
